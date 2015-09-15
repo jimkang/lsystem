0a285dd61de6493ee4f97ed5bb5df4dc80c105a3
Jim Kang <jimkang@gmail.com>
2015-09-15 09:04:23 -0400

    Pulled algae info out into its own fixture file.

--- tests/basictests.js
+++ tests/basictests.js
@@ -1,29 +1,12 @@
 var test = require('tape');
 var LSystem = require('../index');
-
-var expectedAlgaeStates = [
-  'A',
-  'AB',
-  'ABA',
-  'ABAAB',
-  'ABAABABA',
-  'ABAABABAABAAB',
-  'ABAABABAABAABABAABABA',
-  'ABAABABAABAABABAABABAABAABABAABAAB'
-];
-Object.freeze(expectedAlgaeStates);
+var algaeSystem = require('./fixtures/algae-system');
+var _ = require('lodash');
 
 test('Algae test', function algae(t) {
-  var lsys = LSystem({
-    variables: ['A', 'B'],
-    initialState: 'A',
-    rules: {
-      A: 'AB',
-      B: 'A'
-    }
-  });
-
-  expectedAlgaeStates.forEach(testIteration);
+  var lsys = LSystem(_.pick(algaeSystem, 'initialState', 'rules'));
+  
+  algaeSystem.expectedStates.forEach(testIteration);
 
   function testIteration(expectedState, i) {
     t.equal(lsys.getIteration(), 0, `Reports it is on iteration ${i}.`);
